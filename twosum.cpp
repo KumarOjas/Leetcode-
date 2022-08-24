be888a8e83e1ff15eed5dc7804166b3b292e2db9
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2022-08-24 00:54:56 -0700

    Create twosum.cpp

---
+++ twosum.cpp
@@ -0,0 +1,44 @@
+class Solution {
+  
+  public:
+  
+  static bool Compare
+    _(const pair<int,int> &a ,const  pair<int,int> &b){
+        return a.second < b.second;
+        
+    }
+    
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<pair<int,int>>s;
+        
+        for(int i=0; i<nums.size(); i++)
+        {
+            s.push_back({i, nums[i]});
+        }
+        
+        sort(s.begin(), s.end(), Compare_);
+        vector<int>vec;
+        int i=0;
+        int j = s.size()-1;
+        
+        while(i<j)
+        {
+            if( s[i].second + s[j].second == target ) 
+            { 
+                vec.push_back(s[i].first);
+                vec.push_back(s[j].first);
+                return vec;
+            }
+            if(s[i].second + s[j].second > target)
+            {
+                j--;
+            } 
+            else
+            {
+                i++;
+            }
+        }
+        
+        return vec;
+    }
+};
