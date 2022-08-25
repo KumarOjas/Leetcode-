bcb867f5cb7c31039aa0caddd8097024db1a3d7b
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2022-08-24 23:03:14 -0700

    Create Triangleandsquare.cpp

---
+++ Triangleandsquare.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+    int countSquare(int B)
+    {
+	    // code here 
+	    int p = B-2;
+	    
+	    p = p/2;
+	    
+	    return (p*(p+1))/2;
+    }
+};
+
+
+
+// { Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+    	int B,count;
+    	cin>>B;
+    	Solution ob;
+    	count = ob.countSquare(B);
+    	cout<<count<<endl;
+    }
+}  // } Driver Code Ends
