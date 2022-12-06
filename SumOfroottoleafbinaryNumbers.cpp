ab712a14b125ef630ff474b5723a36af2adbe45c
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2022-12-06 00:37:53 -0800

    Create SumOfroottoleafbinaryNumbers.cpp

---
+++ SumOfroottoleafbinaryNumbers.cpp
@@ -0,0 +1,36 @@
+class Solution {
+  
+  public:
+  
+  
+  int helper(TreeNode* root, int curr)
+  
+  {
+   
+    if(root==NULL) return 0;
+        
+    
+    curr = curr*2 + root->val;
+        
+    
+    if(root->left==nullptr && root->right==nullptr){
+    
+      return curr;
+      
+    }
+    
+    
+        return helper(root->right, curr) + helper(root->left, curr);
+    
+  }
+  
+  int sumRootToLeaf(TreeNode* root) {
+     
+    if(root==NULL) return 0;
+    
+    int curr=0;
+    
+    return helper(root, curr);
+    
+  }
+};
